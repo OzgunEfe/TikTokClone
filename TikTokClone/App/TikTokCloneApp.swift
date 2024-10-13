65837abec026a380fa84bb4ddec402e39c2c302b
OzgunEfe <ozgunefe.dev@gmail.com>
2024-10-13 16:52:50 +0100

    Initial Commit

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Ozgun Efe on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
